dfa2ed22c4f47d6bafca0c5fd5d5c1d83966648b
Rodrigo Moran <rigo.moran0@gmail.com>
2016-04-21 15:33:20 -0400

    Added testing methods

--- RamOS/src/RamOS.cpp
+++ RamOS/src/RamOS.cpp
@@ -13,36 +13,38 @@
 //#include "Info.h"
 using namespace std;
 
-void test() {
-	cout << "Starting OS"<<endl;
+void test_pcb() {
+	cout << "Starting PCB Test"<<endl;
+
+	int id;
+	PCB pcb,pcb2;
+	cout << "Process 1 pid: " << pcb.get_pid()<<endl;
+	cout << "Process 1 time: " << pcb.info.get_time()<<endl;
+
+	cout << "Process 2 pid: " << pcb2.get_pid()<<endl;
+	cout << "Process 2 time: " << pcb2.info.get_time()<<endl;
 };
-
-int main() {
-
-	PCB proc1;
+void test_state_manager() {
+	cout << "Staring StateManager Test"<<endl;
+	PCB pcb,pcb1,pcb2;
 	StateManager sm;
-	sm.new_state.push_front(proc1);
+
+	sm.new_state.push_front(pcb);
+	sm.new_state.push_back(pcb2);
+	sm.new_state.push_front(pcb1);
 
 	cout << "The new state list contains: " << sm.new_state.size() << " elements" << endl;
 	for (list<PCB>::iterator it = sm.new_state.begin();
 		 it != sm.new_state.end(); it++) {
 		cout << "Start time: " << (*it).info.get_time() << endl;
 	}
-//	cout << "The 1 element has a start time of " << sm.m_new_state.get_allocator(
-//	cout << "First element in list contains: " << sm.m_new_state.
-//	test();
-//	int id;
-//	PCB pcb, pcb2;
-//
-//	id = pcb.get_pid();
-//
-//	cout << "Process1 PID: " << id<<endl;
-//
-//	pcb2.set_pid(123);
-//	id = pcb2.get_pid();
-//
-//	cout << "Process2 PID: " << id<<endl;
-//
-//	cout << "Process1 start time: " << pcb.info.get_time()<<endl;
+}
+
+int main() {
+
+	test_pcb();
+
+	test_state_manager();
+
 	return 0;
 }
